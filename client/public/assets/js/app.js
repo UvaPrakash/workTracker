8060dcf8e45853468fc4552ec0b0cadca9982fda
unknown <uvaprakash@outlook.com>
2015-10-18 15:07:59 +0530

    Aligned the button in middle. Removed blue border around the button by changing CSS property.

--- client/public/assets/js/app.js
+++ client/public/assets/js/app.js
@@ -1,23 +1,39 @@
 "use strict";
 
-var app = angular.module("trackerapp",['ui.router','ngAnimate','ngTouch']);
+var app = angular.module("trackerapp", ['ui.router', 'ngAnimate', 'ngTouch']);
 
-app.config(["$stateProvider","$urlRouterProvider",function(stateProvider,urlRouterProvider){
+app.config(["$stateProvider", "$urlRouterProvider", function (stateProvider, urlRouterProvider) {
     urlRouterProvider
-    .when('/dashboard','/dashboard/createproject'),
+        .when('/dashboard', '/dashboard/createproject'),
 
-    urlRouterProvider
-    .otherwise('/login'),
-    stateProvider
-    .state("base", {"abstract": !0,url: "",templateUrl: "client/views/base.html"})
-    .state("login", {url: "/login",parent: "base",templateUrl: "client/views/user/login.html",controller: "userController"})
-    .state("registration", {url: "/registration",parent: "base",templateUrl: "client/views/user/registration.html",controller: "userController"})
-    .state("dashboard",{url: "/dashboard",templateUrl : "client/views/dashboard.html", parent: "base",controller  : 'dashboardController'});
+        urlRouterProvider
+        .otherwise('/login'),
+        stateProvider
+        .state("base", {
+            "abstract": !0,
+            url: "",
+            templateUrl: "client/views/base.html"
+        })
+        .state("login", {
+            url: "/login",
+            parent: "base",
+            templateUrl: "client/views/user/login.html",
+            controller: "userController"
+        })
+        .state("registration", {
+            url: "/registration",
+            parent: "base",
+            templateUrl: "client/views/user/registration.html",
+            controller: "userController"
+        })
+        .state("dashboard", {
+            url: "/dashboard",
+            templateUrl: "client/views/dashboard.html",
+            parent: "base",
+            controller: 'dashboardController'
+        });
 }]);
 
-app.controller("dashboardController", ["$scope", "$state", function(r, t) {
+app.controller("dashboardController", ["$scope", "$state", function (r, t) {
     r.$state = t;
 }]);
-         
- 
- 
